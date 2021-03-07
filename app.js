17d06678de5bccb3bd4ee64233b7c7691f5117c9
Maruf Hasan Mujahid <47620872+Mujahid9396@users.noreply.github.com>
2021-03-07 19:27:39 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,5 @@
-// var x = 10;
-// while (x < 15) {
-//   document.write(x + " <br>");
-//   x++;
-// }
 
-// function price(x, y, z) {
-//   var clock = x * 100;
-//   var phone = y * 800;
-//   var laptop = z * 1500;
-//   var total = clock + phone + laptop;
-//   return total;
-// }
 
-// var result = price(3, 2, 12);
-// document.write(result);
-// var faseOne = 10 * 100;
-// var faseTwo = 10 * 80;
-// var lessTwo = faseTwo + faseTwo;
-// var day = 21;
-// if (day <= 10) {
-//   document.write(day * 100);
-// } else if (day <= 20 && day > 10) {
-//   var first = day - 10;
-//   var offerOne = first * 80;
-//   var offerOneTotal = offerOne + faseOne;
-//   document.write(offerOneTotal);
-// } else if (day > 20) {
-//   var second = day - 20;
-//   var offerTwo = second * 50;
-
-//   // document.write(offerTwo);
-
-//   var total = lessTwo + offerTwo;
-//   document.write(total);
-
-//   // var offerTwoTotal = faseOne + faseTwo + offerTwo;
-//   // document.write(offerTwo);
-// }
 
 var lessTenPrice = 100;
 var lessTwentyPrice = 80;
@@ -49,14 +12,18 @@ var day = prompt("Enter Your Day : ");
 if (day <= 10) {
   document.write(day * lessTenPrice);
 }
-if (day <= 20 && day > 10) {
+else if (day <= 20 && day > 10) {
   var countOne = day - 10;
   var lessT = countOne * lessTwentyPrice;
   document.write(lessT + lessTen);
 }
 
-if (day > 20) {
+else if (day > 20) {
   var countTwo = day - 20;
   var countTwoTotal = countTwo * upToTwenty;
   document.write(countTwoTotal + lessTen + lessTwenty);
+}
+
+else {
+document.write("<h1>You have enterd invalid number!</h1>");
 }
